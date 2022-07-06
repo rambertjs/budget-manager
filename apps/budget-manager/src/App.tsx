04f5751f025b3fdf68c21a3dd6c6154733526a23
rambertjs <94582124+rambertjs@users.noreply.github.com>
2022-07-06 11:12:00 -0300

    feat(react): muestra operaciones

--- apps/budget-manager/src/App.tsx
+++ apps/budget-manager/src/App.tsx
@@ -1,8 +1,19 @@
+import { useOperaciones } from './hooks/useOperaciones';
+
 export const App = () => {
+  const operaciones = useOperaciones();
   return (
     <>
-      <h1>Hello</h1>
-      <pre></pre>
+      <h1>Operaciones</h1>
+      <ul>
+        {operaciones.map((op) => (
+          <li key={op.id}>
+            <span className={op.tipo} title={op.fecha}>{`$${
+              op.monto / 100
+            } por ${op.concepto}`}</span>
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
