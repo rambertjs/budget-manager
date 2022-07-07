6f562d8282d6f1fb4c45a6f4ca3a34e303f33bed
rambertjs <94582124+rambertjs@users.noreply.github.com>
2022-07-06 22:51:39 -0300

    feat(react): useOperations hook can return balance

--- apps/budget-manager/src/hooks/useOperations.ts
+++ apps/budget-manager/src/hooks/useOperations.ts
@@ -2,15 +2,20 @@ import { Operation as DBOperation } from '@alkemy-fullstack/prisma-client';
 import { useEffect, useState } from 'react';
 
 type Operation = Omit<DBOperation, 'date'> & { date: string };
+type Params = { withBalance: boolean };
 
-export const useOperations = () => {
+export const useOperations = ({ withBalance }: Params) => {
   const [operations, setOperations] = useState<Operation[]>([]);
+  const [balance, setBalance] = useState();
 
   useEffect(() => {
-    fetch('/api/operations')
+    fetch(`/api/operations${withBalance && '?withBalance'}`)
       .then((res) => res.json())
-      .then(setOperations);
-  }, []);
+      .then(({ operations, balance }) => {
+        setOperations(operations);
+        if (balance) setBalance(balance);
+      });
+  }, [withBalance]);
 
-  return operations;
+  return { operations, ...(withBalance && { balance }) };
 };
