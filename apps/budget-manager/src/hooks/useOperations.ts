9549cd79474188d83fa4cec3e4d83927958f5aff
rambertjs <94582124+rambertjs@users.noreply.github.com>
2022-07-07 01:03:14 -0300

    feat(react): useOperations now supports take

--- apps/budget-manager/src/hooks/useOperations.ts
+++ apps/budget-manager/src/hooks/useOperations.ts
@@ -6,10 +6,11 @@ type Balance = {
   income: number;
   expenses: number;
 };
-type Params<T extends boolean> = { withBalance: T };
+type Params<T extends boolean> = { withBalance: T; take?: number };
 
 export const useOperations = <T extends boolean>({
   withBalance,
+  take,
 }: Params<T>): T extends true
   ? {
       operations: Operation[];
@@ -22,13 +23,17 @@ export const useOperations = <T extends boolean>({
   const [balance, setBalance] = useState<Balance>({} as Balance);
 
   useEffect(() => {
-    fetch(`/api/operations${withBalance && '?withBalance'}`)
+    const params = new URLSearchParams();
+    withBalance && params.append('withBalance', '1');
+    take && params.append('take', `${take}`);
+
+    fetch(`/api/operations?${params}`)
       .then((res) => res.json())
       .then(({ operations, balance }) => {
         setOperations(operations);
         if (balance) setBalance(balance);
       });
-  }, [withBalance]);
+  }, [withBalance, take]);
 
   return { operations, ...(withBalance && { balance }) };
 };
