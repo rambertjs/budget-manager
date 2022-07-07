aefc76372c7f1a5267059919c688822e5f1aa812
rambertjs <94582124+rambertjs@users.noreply.github.com>
2022-07-06 22:58:40 -0300

    fix(react): useOperations now has proper typings

--- apps/budget-manager/src/hooks/useOperations.ts
+++ apps/budget-manager/src/hooks/useOperations.ts
@@ -2,11 +2,15 @@ import { Operation as DBOperation } from '@alkemy-fullstack/prisma-client';
 import { useEffect, useState } from 'react';
 
 type Operation = Omit<DBOperation, 'date'> & { date: string };
+type Balance = {
+  income: number;
+  expenses: number;
+};
 type Params = { withBalance: boolean };
 
 export const useOperations = ({ withBalance }: Params) => {
   const [operations, setOperations] = useState<Operation[]>([]);
-  const [balance, setBalance] = useState();
+  const [balance, setBalance] = useState<Balance>();
 
   useEffect(() => {
     fetch(`/api/operations${withBalance && '?withBalance'}`)
