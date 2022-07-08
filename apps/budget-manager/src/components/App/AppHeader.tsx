4f6d34e21492cb3e9889b24ca3245697004c7c36
rambertjs <94582124+rambertjs@users.noreply.github.com>
2022-07-08 06:12:10 -0300

    fix(react): fixed AppHeader styling attribute

--- apps/budget-manager/src/components/App/AppHeader.tsx
+++ apps/budget-manager/src/components/App/AppHeader.tsx
@@ -28,10 +28,7 @@ export const AppHeader = ({ toggleNavbar, burgerStatus }: Props) => {
         <UnstyledButton component={Link} to="/" style={{ height: '100%' }}>
           <Group style={{ height: '100%' }}>
             <img src="assets/logo.png" alt="" style={{ maxHeight: '100%' }} />
-            <Title
-              px={15}
-              styles={(theme) => ({ color: theme.colors['gray'][7] })}
-            >
+            <Title px={15} sx={(theme) => ({ color: theme.colors['gray'][7] })}>
               Tu Presupuesto
             </Title>
           </Group>
