e10becb95f57cd003bc2536488c1e1e7c5493d5b
rambertjs <94582124+rambertjs@users.noreply.github.com>
2022-07-11 11:26:47 -0300

    fix(api): pass error to middleware instead of throwing

--- apps/api/src/auth/controller.ts
+++ apps/api/src/auth/controller.ts
@@ -1,9 +1,9 @@
 import { CustomError } from './../errorMiddleware';
 export const AuthController = {
-  async login(_req, _res) {
-    throw new CustomError(501, 'not implemented');
+  async login(_req, _res, next) {
+    next(new CustomError(501, 'not implemented'));
   },
-  async register(_req, _res) {
-    throw new CustomError(501, 'not implemented');
+  async register(_req, _res, next) {
+    next(new CustomError(501, 'not implemented'));
   },
 };
