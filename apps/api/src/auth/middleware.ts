21ebad1bfbe87927c3325ed7e823c0b820abf5a3
rambertjs <94582124+rambertjs@users.noreply.github.com>
2022-07-12 02:55:32 -0300

    feat(api): add auth middleware

---
+++ apps/api/src/auth/middleware.ts
@@ -0,0 +1,15 @@
+import { CustomError } from './../errorMiddleware';
+import * as jwt from 'jsonwebtoken';
+export const AuthMiddleware = (req, res, next) => {
+  const token = req.headers['authorization'].split(' ')[1];
+  if (!token) {
+    throw new CustomError(401, 'No token provided');
+  }
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      throw new CustomError(401, 'Invalid token provided');
+    }
+    req.userId = decoded.id;
+    next();
+  });
+};
