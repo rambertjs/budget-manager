6dee5b56a21a4a67ea9331d4bad9337136d1c825
rambertjs <94582124+rambertjs@users.noreply.github.com>
2022-07-12 03:11:09 -0300

    fix(api): handle undefined token

--- apps/api/src/auth/middleware.ts
+++ apps/api/src/auth/middleware.ts
@@ -1,7 +1,7 @@
 import { CustomError } from './../errorMiddleware';
 import * as jwt from 'jsonwebtoken';
 export const AuthMiddleware = (req, res, next) => {
-  const token = req.headers['authorization'].split(' ')[1];
+  const token = req.headers['authorization']?.split(' ')[1];
   if (!token) {
     throw new CustomError(401, 'No token provided');
   }
