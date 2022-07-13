d2a24b5ad27f820315b4c11ca444bf3ef7dc23a1
rambertjs <94582124+rambertjs@users.noreply.github.com>
2022-07-12 21:49:12 -0300

    feat(react): useAuth hook

---
+++ apps/budget-manager/src/hooks/useAuth.ts
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+interface IResponse {
+  token: string;
+  email: string;
+}
+
+export const useAuth = () => {
+  const login = (email: string, password: string) => {
+    axios
+      .post<IResponse>('/api/auth/login', { email, password })
+      .then(({ data }) => {
+        localStorage.setItem('token', data.token);
+        localStorage.setItem('email', data.email);
+      })
+      .catch(({ response: { data } }) => {
+        console.error(data);
+      });
+  };
+  const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('email');
+  };
+  const isAuthenticated = () => {
+    return !!localStorage.getItem('token');
+  };
+  const user = () => {
+    return localStorage.getItem('email');
+  };
+  return { login, logout, isAuthenticated, user };
+};
