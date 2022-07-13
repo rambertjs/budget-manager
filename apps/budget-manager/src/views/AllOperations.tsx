7f6da2d532b303f574f2728d5979ff67f7ba00ff
rambertjs <94582124+rambertjs@users.noreply.github.com>
2022-07-12 23:35:42 -0300

    feat(react): AllOperations view

--- apps/budget-manager/src/views/AllOperations.tsx
+++ apps/budget-manager/src/views/AllOperations.tsx
@@ -1,8 +1,24 @@
+import { LoadingOverlay, Pagination, Stack } from '@mantine/core';
+import { useState } from 'react';
+import { OperationsTable } from '../components/Operations/OperationsTable';
+import { useOperations } from '../hooks/useOperations';
+
 export const AllOperations = () => {
+  const [page, setPage] = useState(1);
+  const { data, isFetching } = useOperations(page);
+
   return (
     <div>
       <h1>Todas las operaciones</h1>
-      {/* not implemented */}
+      <Stack sx={{ position: 'relative' }}>
+        <LoadingOverlay visible={isFetching} />
+        <OperationsTable operations={data?.operations || []} />
+        <Pagination
+          page={page}
+          total={data?.totalPages || 1}
+          onChange={setPage}
+        />
+      </Stack>
     </div>
   );
 };
