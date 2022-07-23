70bd79df3e2d6353cd7ca204732687ba0269b8ad
rambertjs <94582124+rambertjs@users.noreply.github.com>
2022-07-23 20:15:48 -0300

    fix: always serve built frontend

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -19,13 +19,11 @@ app.use('/api/operations', [AuthMiddleware, OperationsRouter]);
 app.use(prismaErrorMiddleware);
 app.use(customErrorMiddleware);
 
-if (process.env.NODE_ENV === 'production') {
-  const BUILD_PATH = path.join(__dirname, '../budget-manager/src');
-  app.use(express.static(BUILD_PATH));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(BUILD_PATH, 'index.html'));
-  });
-}
+const BUILD_PATH = path.join(__dirname, '../budget-manager');
+app.use(express.static(BUILD_PATH));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(BUILD_PATH, 'index.html'));
+});
 
 const port = process.env.PORT || 3333;
 const server = app.listen(port, () => {
